d36a0016272d396b61366666a03292d9001bdb45
Elhanes <37682794+Elhanes@users.noreply.github.com>
2020-01-14 21:08:39 +0900

    Seperate declaration part and implementation part. Fixed both pop function. Error handling at pop function.

--- dataStructure/singleLinkedList.h
+++ dataStructure/singleLinkedList.h
@@ -1,5 +1,5 @@
 #pragma once
-//#include <iostream>
+#include <iostream>
 
 template<class T> class singleLinkedList_Node;
 template<class T> class singleLinkedList;
@@ -7,7 +7,6 @@ template<class T> class singleLinkedList;
 template <class T>
 class singleLinkedList_Node
 {
-
 	friend class singleLinkedList<T>;
 
 private:
@@ -25,105 +24,140 @@ private:
 	int num_node;
 
 public:
-	singleLinkedList()
-	{
-		head = tail = new singleLinkedList_Node<T>(NULL, nullptr);
-		num_node = 0;
-	}
+	singleLinkedList();
+	~singleLinkedList();
 	
-	void push_front(T value)
-	{
-		singleLinkedList_Node<T>* node = new singleLinkedList_Node<T>(value, nullptr);
-
-		if (head->next == nullptr)
-		{
-			head->next = node;
-			tail = node;
-		}
-		else
-		{
-			singleLinkedList_Node<T>* tmp = head->next;
-			head->next = node;
-			head->next->next = tmp;
-		}
-
-		num_node++;
-	}
-
-	void push_back(T value)
-	{
-		singleLinkedList_Node<T>* node = new singleLinkedList_Node<T>(value, nullptr);
-
-		if (head->next != nullptr) tail->next = node;
-
-		tail = node;
-		num_node++;
-	}
-
-	T pop_front()
-	{
-		T val;
-
-		if (head->next != nullptr) // check list is not empty
-		{
-			singleLinkedList_Node<T>* tmp = head->next;
-			head->next = head->next->next;
-			val = tmp->value;
-			delete tmp;
-			num_node--;
-		}
-
-		// need to add error case - be called when the list is empty
-
-		return val;
-	}
-
-	T pop_back()
-	{
-		T val;
-
-		if (head->next != nullptr) // check list is not empty
-		{
-			singleLinkedList_Node<T>* tmp = tail;
-			singleLinkedList_Node<T>* scan = head;
-
-			while (scan->next != tail) scan = scan->next;
-
-			scan->next = nullptr;
-			tail = scan;
-			val = tmp->value;
-			delete tmp;
-			num_node--;
-		}
-
-		// need to add error case - be called when the list is empty
-
-		return val;
-	}
-
-	T get_head()
-	{
-		if (head->next != nullptr) return head->next->value;
-		else return 0;
-	}
-
-	T get_tail()
-	{
-		if (head->next != nullptr) return tail->value;
-		else return 0;
-	}
-
-	int size() { return num_node; }
-	
-	bool empty()
-	{
-		if (num_node == 0) return 1;
-		else return 0;
-	}
-
-	~singleLinkedList()
-	{
-		while (head->next != nullptr) pop_front();
-		delete head;
-	}
+	void push_front(T value);
+	void push_back(T value);
+	void pop_front();
+	void pop_back();
+	T get_head();
+	T get_tail();
+	int size();
+	bool empty();
 };
+
+// inline function is automatically apply, on modern c++ version
+template <class T>
+singleLinkedList<T>::singleLinkedList()
+{
+	head = tail = new singleLinkedList_Node<T>(NULL, nullptr);
+	num_node = 0;
+}
+
+template <class T>
+void singleLinkedList<T>::push_front(T value)
+{
+	singleLinkedList_Node<T>* node = new singleLinkedList_Node<T>(value, nullptr);
+
+	if (head->next == nullptr)
+	{
+		head->next = node;
+		tail = node;
+	}
+	else
+	{
+		singleLinkedList_Node<T>* tmp = head->next;
+		head->next = node;
+		head->next->next = tmp;
+	}
+
+	num_node++;
+}
+
+template <class T>
+void singleLinkedList<T>::push_back(T value)
+{
+	singleLinkedList_Node<T>* node = new singleLinkedList_Node<T>(value, nullptr);
+
+	if (head->next == nullptr)
+	{
+		head->next = node;
+		tail = node;
+	}
+	else tail->next = node;
+
+	tail = node;
+	num_node++;
+}
+
+template <class T>
+void singleLinkedList<T>::pop_front()
+{
+	if (head->next != nullptr) // check list is not empty
+	{
+		singleLinkedList_Node<T>* tmp = head->next;
+		head->next = head->next->next;
+		delete tmp;
+		num_node--;
+	}
+	else std::cout << "singleLinkedList::pop_front() : List is empty\n";
+}
+
+template <class T>
+void singleLinkedList<T>::pop_back()
+{
+	if (head->next != nullptr) // check list is not empty
+	{
+		singleLinkedList_Node<T>* tmp = tail;
+		singleLinkedList_Node<T>* scan = head;
+
+		while (scan->next != tail) scan = scan->next;
+
+		scan->next = nullptr;
+		tail = scan;
+		delete tmp;
+		num_node--;
+	}
+	else std::cout << "singleLinkedList::pop_front() : List is empty\n";
+}
+
+template <class T>
+T singleLinkedList<T>::get_head()
+{
+	if (head->next != nullptr) return head->next->value;
+	else return 0;
+}
+
+template <class T>
+T singleLinkedList<T>::get_tail()
+{
+	if (head->next != nullptr) return tail->value;
+	else return 0;
+}
+
+template <class T>
+int singleLinkedList<T>::size() 
+{
+	return num_node;
+}
+
+template <class T>
+bool singleLinkedList<T>::empty()
+{
+	if (num_node == 0) return 1;
+	else return 0;
+}
+
+template <class T>
+singleLinkedList<T>::~singleLinkedList()
+{
+	while (head->next != nullptr) pop_front();
+	delete head;
+}
+
+/* Implementation of the class
+
+List node just include two datas, one is value, and other one is pointer of the next node.
+This list just have one direction, but include head and tail node info. Only just these.
+pop and get function is seperated. -> ..Same reason why STL stack and queue`s top(front) and pop function is seperated.
+pop and get function is both side, head and tail.
+But in pop_back function, for access to the node behind the tail, function will scan head to tail, because node only have one direction.
+empty function return only status 0 and 1(bool type).
+size function return int type constant.
+Class template is applied.
+
+next work : check the memory lick, and error handling on the pop function
+
+written by elhanes
+*/
--- dataStructure/test.cpp
+++ dataStructure/test.cpp
@@ -3,51 +3,61 @@
 
 int main()
 {
-	singleLinkedList<char> sll;
+	singleLinkedList<char> sll_char;
 	singleLinkedList<int> sll_int;
+	singleLinkedList<float> sll_float;
 
-	sll.push_front('a');
-	sll.push_front('b');
-	sll.push_front('c');
+	//insert char to list
+	sll_char.push_front('a');
+	sll_char.push_front('b');
+	sll_char.push_front('c');
+	sll_char.push_back('d');
+	sll_char.push_back('e');
 
-	sll.push_back('d');
-	sll.push_back('e');
+	//insert int to list
+	for (int i = 0; i < 5; i++) sll_int.push_front(i);
+	for (int i = 5; i < 10; i++) sll_int.push_back(i);
 
-	sll_int.push_front(1);
-	sll_int.push_front(2);
-	sll_int.push_front(3);
+	//insert float to list
+	for (int i = 1; i < 6; i++) sll_float.push_front(i + 0.1);
 
-	sll_int.push_back(4);
-	sll_int.push_back(5);
+	//test size
+	std::cout << "char list size : " << sll_char.size() << ", int list size : " << sll_int.size() << ", float list size : " << sll_float.size() << "\n";
+	std::cout << "----------------------------\n";
 
-	std::cout << "size of list is " << sll.size() << "\n";
-	if (!sll.empty()) std::cout << "This single linked list is not empty\n";
-	else std::cout << "This single linked list is empty\n";
-
-	std::cout << "head of list is " << sll.get_head() << "\n";
-	std::cout << "tail of list is " << sll.get_tail() << "\n";
-
-	std::cout << "-------------------------List contents----------------------------\n";
-
-	while (!sll.empty())
+	//test pop_front and get_head
+	while (!sll_char.empty())
 	{
-		std::cout << sll.pop_front() << " ";
+		std::cout << sll_char.get_head() << " ";
+		sll_char.pop_front();
 	}
-	std::cout << "\n";
 
+	std::cout << "\n----------------------------\n";
+
+	//test pop_back and get_tail
 	while (!sll_int.empty())
 	{
-		std::cout << sll_int.pop_front() << " ";
+		std::cout << sll_int.get_head() << " ";
+		std::cout << sll_int.get_tail() << " ";
+		sll_int.pop_front();
+		sll_int.pop_back();
 	}
+
 	std::cout << "\n";
-	std::cout << "------------------------------------------------------------------\n";
 
-	std::cout << "size of list is " << sll.size() << "\n";
-	if (!sll.empty()) std::cout << "This single linked list is not empty\n";
-	else std::cout << "This single linked list is empty\n";
+	//test empty
+	if (sll_int.empty()) std::cout << "int list is empty\n";
+	else std::cout << "int list is not empty\n";
 
-	std::cout << "head of list is " << sll.get_head() << "\n";
-	std::cout << "tail of list is " << sll.get_tail() << "\n";
+	std::cout << "----------------------------\n";
+
+	while (!sll_float.empty())
+	{
+		std::cout << sll_float.get_head() << " ";
+		sll_float.pop_front();
+	}
+
+	std::cout << "\n----------------------------";
 
 	return 0;
 }
